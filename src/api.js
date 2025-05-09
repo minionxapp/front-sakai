857f28e5b9af471f998bef8d2c8dbbefbcfea0e2
Mugi <minionxapp@gmail.com>
2025-05-09 13:13:58 +0700

    deploy

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// axios.defaults.withCredentials = true;
+// 1111111
 const custumFetch = axios.create({
     // baseURL :'/api/v1' 
     baseURL :'https://backend02-gamma.vercel.app/api/v1'
